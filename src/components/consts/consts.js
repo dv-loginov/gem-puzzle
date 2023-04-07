20e73771f0228db7095d43ad6e3ffc31142c4f5d
dv-loginov <dilog@rambler.ru>
2023-04-07 12:43:00 +0300

    feat: add listener to buttons

--- src/components/consts/consts.js
+++ src/components/consts/consts.js
@@ -1,24 +1,20 @@
-// export const checkTheme = {
-//   checker: ".checkbox-theme",
-//   icon: ".setting__theme",
-// }
-//
-// export const checkMute = {
-//   checker: ".checkbox-mute",
-//   icon: ".setting__mute",
-// }
-
 export const check = {
   themeCheck: {
-    nodeCheck: document.querySelector(".checkbox-theme"),
+    nodeCheck: document.querySelector(".checkbox_theme"),
     nodeIco: document.querySelector(".setting__theme"),
     icoOn: "light_mode",
     icoOff: "dark_mode",
   },
   muteCheck: {
-    nodeCheck: document.querySelector(".checkbox-mute"),
+    nodeCheck: document.querySelector(".checkbox_mute"),
     nodeIco: document.querySelector(".setting__mute"),
     icoOn:"volume_off",
     icoOff: "volume_up",
   },
+};
+
+export const button = {
+  playNode: document.querySelector(".button_play"),
+  replayNode: document.querySelector(".button_replay"),
+  pauseNode:document.querySelector(".button_pause"),
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import './index.css';
-import { check } from '../components/consts/consts.js';
+import { button, check } from '../components/consts/consts.js';
 import { changeClass, icoChange, onListener } from '../components/utils/utils';
 
 const body = document.querySelector('body');
 
-const {themeCheck, muteCheck} = check;
+const { themeCheck, muteCheck } = check;
+const { playNode: play, replay,pause } = button;
 
 onListener(themeCheck.nodeCheck, 'change', handleChangeTheme);
 onListener(muteCheck.nodeCheck, 'change', handleChangeMute);
+
+onListener(button.playNode, 'click', handleBtnPlay);
+onListener(button.replayNode, 'click', handleBtnReplay);
+onListener(button.pauseNode, 'click', handleBtnPause);
 
 function handleChangeMute(instance) {
   instance.checked
@@ -24,3 +29,17 @@ function handleChangeTheme(instance) {
     changeClass(body, 'theme__light', 'theme__dark');
   }
 }
+
+function handleBtnPlay(){
+  console.log('handleBtnPlay');
+
+}
+
+function handleBtnReplay(){
+  console.log('handleBtnReplay');
+}
+
+function handleBtnPause(){
+  console.log('handleBtnPause');
+}
+
